994314c108704092c6d817b08e158fb8d0ce8f22
builtbyjosh <pc.refuge@gmail.com>
2022-01-20 21:05:15 -0600

    first commit

---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const TrackListScreen = () => {
+  return (
+    <View>
+      <Text>Track List Screen</Text>
+    </View>
+  );
+};
+
+export default TrackListScreen;
+
+const styles = StyleSheet.create({});
