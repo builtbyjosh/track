d110574e28b2384cf0bdeaf5c30372463935e4b8
builtbyjosh <pc.refuge@gmail.com>
2022-01-20 21:24:08 -0600

    set up navigation

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { StyleSheet, Text, View } from 'react-native';
 import AccountScreen from './src/screens/AccountScreen';
 import SigninScreen from './src/screens/SigninScreen';
@@ -7,15 +10,22 @@ import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
 
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen,
+  }),
+  mainFlow: createBottomTabNavigator({
+    TrackListFlow: createStackNavigator({
+      TrackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen,
+    }),
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen,
+  }),
+});
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default createAppContainer(switchNavigator)
 
 const styles = StyleSheet.create({
   container: {
@@ -25,4 +35,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import React from 'react';
 
-const TrackListScreen = () => {
+const TrackListScreen = ({navigation}) => {
   return (
     <View>
       <Text>Track List Screen</Text>
+      <Button
+        title="Go to Track Detail"
+        onPress={() => {
+          navigation.navigate('TrackDetail');
+        }}
+      />
     </View>
   );
 };
