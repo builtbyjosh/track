8b6799b0cd9d2d34fbc5f9a1bf8b9a9c596e1d4d
builtbyjosh <pc.refuge@gmail.com>
2022-01-24 22:27:13 -0600

    styling changes

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,25 +1,46 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
-import React from 'react';
+import React, { useState} from 'react';
 import Spacer from '../components/Spacer';
+import axios from 'axios'
+
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState();
+
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
-        <Text h3>Signup for Tracker</Text>
+        <Text style={styles.header} h3>Signup for Tracker</Text>
       </Spacer>
-      <Input label="Email" />
+      <Input label="Email" value={email} onChangeText={setEmail} autoCapitalize={false} autoCorrect={false}/>
       <Spacer />
-      <Input label="Password" />
+      <Input label="Password" secureTextEntry value={password} onChangeText={setPassword} autoCapitalize={false} autoCorrect={false}/>
 
       <Spacer>
         <Button title="Signup" />
       </Spacer>
-    </>
+    </View>
   );
+};
+
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
 };
 
 export default SignupScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 50,
+       
+  },
+  header: {
+    marginBottom: 50
+  }
+});
