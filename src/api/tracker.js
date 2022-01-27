469fb3ceb7aec67daee18cea3f445479d4b77d50
builtbyjosh <pc.refuge@gmail.com>
2022-01-26 21:42:32 -0600

    update ngrok url

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://e2d5-2601-241-80-9410-1db6-981a-79f7-6154.ngrok.io',
+  baseURL: ' http://87c6-2601-241-80-9410-314a-54d1-3323-c875.ngrok.io',
 });
